9de678245c47528687ea580bed9396712aca3c65
TCdotmake <tommyjchen@hotmail.com>
2022-08-25 22:59:33 -0400

    number entry working as a string and displays correctly

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ function divide(a, b) {
 function operate(operation, a, b) {
   return operation(a, b);
 }
+
+let aVal = 0;
+
+const numList = document.getElementsByClassName("num");
+const display = document.getElementById("display");
+console.log("display", display);
+
+console.log("numList", numList);
+for (let i = 0; i < numList.length; i++) {
+  const element = numList[i];
+  element.setAttribute("data-value", element.innerHTML);
+  element.addEventListener("click", (e) => {
+    const dArr = display.dataset.value.split("");
+    if (!dArr.includes(".") || e.target.dataset.value !== ".")
+      dArr.push(e.target.dataset.value);
+    if (dArr.length > 1 && dArr[0] === "0" && dArr[1] !== ".") {
+      dArr.splice(0, 1);
+      console.log("dArr", dArr);
+    }
+    display.dataset.value = dArr.join("");
+    display.innerText = display.dataset.value;
+  });
+}
